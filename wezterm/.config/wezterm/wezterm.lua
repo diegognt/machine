a3b2c09b7bab609a8f3e932703a2f64d75967689
Diego Navarro <code@diegognt.dev>
2025-03-04 19:35:05 -0500

    updates wezterm config file

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -6,7 +6,7 @@
 ---   \_/\_/\___/__|\__\___|_| |_|_|_|
 ---
 --- A Wezterm config file
-
+--
 local wezterm = require("wezterm")
 local act = wezterm.action
 
@@ -18,147 +18,144 @@ if wezterm.config_builder then config = wezterm.config_builder() end
 -- Settings
 config.color_scheme = "nightfox"
 config.font = wezterm.font_with_fallback({
+  -- {
+  --   family = "Monaspace Argon",
+  --   scale = 1,
+  --   weight = "Regular",
+  --   harfbuzz_features = { 'calt', 'liga', 'dlig', 'ss01', 'ss02', 'ss03', 'ss04'},
+  -- },
   {
-    family = "Monaspace Argon",
-    scale = 1.30,
-    weight = "Medium",
-    harfbuzz_features = { 'calt', 'liga', 'dlig', 'ss01', 'ss02', 'ss03', 'ss04'},
-  },
-  {
-    family = "JetBrainsMono Nerd Font",
-    scale = 1.30,
-    weight = "Medium",
+    family = "JetBrains Mono",
+    scale = 1,
+    weight = "Regular",
   },
 })
 
-config.font_rules = {
-  {
-    intensity = 'Bold',
-    italic = true,
-    font = wezterm.font {
-      family = 'Monaspace Radon',
-      weight = 'Bold',
-      style = 'Italic',
-      scale = 1.33,
-      harfbuzz_features = { 'calt', 'liga', 'dlig', 'ss01', 'ss02', 'ss03', 'ss04'},
-    },
-  },
-  {
-    italic = true,
-    intensity = 'Half',
-    font = wezterm.font {
-      family = 'Monaspace Radon',
-      weight = 'DemiBold',
-      style = 'Italic',
-      scale = 1.33,
-      harfbuzz_features = { 'calt', 'liga', 'dlig', 'ss01', 'ss02', 'ss03', 'ss04'},
-    },
-  },
-  {
-    italic = true,
-    intensity = 'Normal',
-    font = wezterm.font {
-      family = 'Monaspace Radon',
-      style = 'Italic',
-      weight = 'Medium',
-      scale = 1.33,
-      harfbuzz_features = { 'calt', 'liga', 'dlig', 'ss01', 'ss02', 'ss03', 'ss04'},
-    },
-  },
-}
+-- config.font_rules = {
+--   {
+--     intensity = 'Bold',
+--     italic = true,
+--     font = wezterm.font {
+--       family = 'Monaspace Radon',
+--       weight = 'Bold',
+--       style = 'Italic',
+--       harfbuzz_features = { 'calt', 'liga', 'dlig', 'ss01', 'ss02', 'ss03', 'ss04'},
+--     },
+--   },
+--   {
+--     italic = true,
+--     intensity = 'Half',
+--     font = wezterm.font {
+--       family = 'Monaspace Radon',
+--       weight = 'DemiBold',
+--       style = 'Italic',
+--       harfbuzz_features = { 'calt', 'liga', 'dlig', 'ss01', 'ss02', 'ss03', 'ss04'},
+--     },
+--   },
+--   {
+--     italic = true,
+--     intensity = 'Normal',
+--     font = wezterm.font {
+--       family = 'Monaspace Radon',
+--       style = 'Italic',
+--       weight = 'Medium',
+--       harfbuzz_features = { 'calt', 'liga', 'dlig', 'ss01', 'ss02', 'ss03', 'ss04'},
+--     },
+--   },
+-- }
 
 config.window_background_opacity = 1
-config.window_decorations = "RESIZE"
+config.window_decorations = "NONE"
 config.window_close_confirmation = "AlwaysPrompt"
 config.scrollback_lines = 3000
 config.default_workspace = "main"
 
--- Keys
-config.leader = {
-  key = "a",
-  mods = "CTRL",
-  timeout_milliseconds = 1000
-}
-config.keys = {
-  -- Send C-a when pressing C-a twice
-  { key = "a",          mods = "LEADER|CTRL", action = act.SendKey { key = "a", mods = "CTRL" } },
-  { key = "c",          mods = "LEADER",      action = act.ActivateCopyMode },
-  { key = "phys:Space", mods = "LEADER",      action = act.ActivateCommandPalette },
-
-  -- Pane keybindings
-  { key = "s",          mods = "LEADER",      action = act.SplitVertical { domain = "CurrentPaneDomain" } },
-  { key = "v",          mods = "LEADER",      action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
-  { key = "q",          mods = "LEADER",      action = act.CloseCurrentPane { confirm = true } },
-  { key = "h",          mods = "LEADER",      action = act.ActivatePaneDirection("Left") },
-  { key = "j",          mods = "LEADER",      action = act.ActivatePaneDirection("Down") },
-  { key = "k",          mods = "LEADER",      action = act.ActivatePaneDirection("Up") },
-  { key = "l",          mods = "LEADER",      action = act.ActivatePaneDirection("Right") },
-  { key = "z",          mods = "LEADER",      action = act.TogglePaneZoomState },
-  { key = "o",          mods = "LEADER",      action = act.RotatePanes "Clockwise" },
-
-  -- We can make separate keybindings for resizing panes
-  -- But Wezterm offers custom "mode" in the name of "KeyTable"
-  { key = "r",          mods = "LEADER",      action = act.ActivateKeyTable { name = "resize_pane", one_shot = false } },
-
-  -- Tab keybindings
-  { key = "t",          mods = "LEADER",      action = act.SpawnTab("CurrentPaneDomain") },
-  { key = "[",          mods = "LEADER",      action = act.ActivateTabRelative(-1) },
-  { key = "]",          mods = "LEADER",      action = act.ActivateTabRelative(1) },
-  { key = "n",          mods = "LEADER",      action = act.ShowTabNavigator },
-  {
-    key = "e",
-    mods = "LEADER",
-    action = act.PromptInputLine {
-      description = wezterm.format {
-        { Attribute = { Intensity = "Bold" } },
-        { Foreground = { AnsiColor = "Fuchsia" } },
-        { Text = "Renaming Tab Title...:" },
-      },
-      action = wezterm.action_callback(function(window, pane, line)
-        if line then
-          window:active_tab():set_title(line)
-        end
-      end)
-    }
-  },
-  -- Key table for moving tabs around
-  { key = "m", mods = "LEADER",       action = act.ActivateKeyTable { name = "move_tab", one_shot = false } },
-  -- Or shortcuts to move tab w/o move_tab table. SHIFT is for when caps lock is on
-  { key = "{", mods = "LEADER|SHIFT", action = act.MoveTabRelative(-1) },
-  { key = "}", mods = "LEADER|SHIFT", action = act.MoveTabRelative(1) },
-
-  -- Lastly, workspace
-  { key = "w", mods = "LEADER",       action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" } },
-}
-
--- I can use the tab navigator (LDR t), but I also want to quickly navigate tabs with index
-for i = 1, 9 do
-  table.insert(config.keys, {
-    key = tostring(i),
-    mods = "LEADER",
-    action = act.ActivateTab(i - 1)
-  })
-end
-
-config.key_tables = {
-  resize_pane = {
-    { key = "h",      action = act.AdjustPaneSize { "Left", 1 } },
-    { key = "j",      action = act.AdjustPaneSize { "Down", 1 } },
-    { key = "k",      action = act.AdjustPaneSize { "Up", 1 } },
-    { key = "l",      action = act.AdjustPaneSize { "Right", 1 } },
-    { key = "Escape", action = "PopKeyTable" },
-    { key = "Enter",  action = "PopKeyTable" },
-  },
-  move_tab = {
-    { key = "h",      action = act.MoveTabRelative(-1) },
-    { key = "j",      action = act.MoveTabRelative(-1) },
-    { key = "k",      action = act.MoveTabRelative(1) },
-    { key = "l",      action = act.MoveTabRelative(1) },
-    { key = "Escape", action = "PopKeyTable" },
-    { key = "Enter",  action = "PopKeyTable" },
-  }
-}
-
+-- -- Keys
+-- config.leader = {
+--   key = "a",
+--   mods = "CTRL",
+--   timeout_milliseconds = 1000
+-- }
+-- config.keys = {
+--   -- Send C-a when pressing C-a twice
+--   { key = "a",          mods = "LEADER|CTRL", action = act.SendKey { key = "a", mods = "CTRL" } },
+--   { key = "c",          mods = "LEADER",      action = act.ActivateCopyMode },
+--   { key = "phys:Space", mods = "LEADER",      action = act.ActivateCommandPalette },
+--
+--   -- Pane keybindings
+--   { key = "s",          mods = "LEADER",      action = act.SplitVertical { domain = "CurrentPaneDomain" } },
+--   { key = "v",          mods = "LEADER",      action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
+--   { key = "q",          mods = "LEADER",      action = act.CloseCurrentPane { confirm = true } },
+--   { key = "h",          mods = "LEADER",      action = act.ActivatePaneDirection("Left") },
+--   { key = "j",          mods = "LEADER",      action = act.ActivatePaneDirection("Down") },
+--   { key = "k",          mods = "LEADER",      action = act.ActivatePaneDirection("Up") },
+--   { key = "l",          mods = "LEADER",      action = act.ActivatePaneDirection("Right") },
+--   { key = "z",          mods = "LEADER",      action = act.TogglePaneZoomState },
+--   { key = "o",          mods = "LEADER",      action = act.RotatePanes "Clockwise" },
+--
+--   -- We can make separate keybindings for resizing panes
+--   -- But Wezterm offers custom "mode" in the name of "KeyTable"
+--   { key = "r",          mods = "LEADER",      action = act.ActivateKeyTable { name = "resize_pane", one_shot = false } },
+--
+--   -- Tab keybindings
+--   { key = "t",          mods = "LEADER",      action = act.SpawnTab("CurrentPaneDomain") },
+--   { key = "[",          mods = "LEADER",      action = act.ActivateTabRelative(-1) },
+--   { key = "]",          mods = "LEADER",      action = act.ActivateTabRelative(1) },
+--   { key = "n",          mods = "LEADER",      action = act.ShowTabNavigator },
+--   {
+--     key = "e",
+--     mods = "LEADER",
+--     action = act.PromptInputLine {
+--       description = wezterm.format {
+--         { Attribute = { Intensity = "Bold" } },
+--         { Foreground = { AnsiColor = "Fuchsia" } },
+--         { Text = "Renaming Tab Title...:" },
+--       },
+--       action = wezterm.action_callback(function(window, pane, line)
+--         if line then
+--           window:active_tab():set_title(line)
+--         end
+--       end)
+--     }
+--   },
+--   -- Key table for moving tabs around
+--   { key = "m", mods = "LEADER",       action = act.ActivateKeyTable { name = "move_tab", one_shot = false } },
+--   -- Or shortcuts to move tab w/o move_tab table. SHIFT is for when caps lock is on
+--   { key = "{", mods = "LEADER|SHIFT", action = act.MoveTabRelative(-1) },
+--   { key = "}", mods = "LEADER|SHIFT", action = act.MoveTabRelative(1) },
+--
+--   -- Lastly, workspace
+--   { key = "w", mods = "LEADER",       action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" } },
+-- }
+--
+-- -- I can use the tab navigator (LDR t), but I also want to quickly navigate tabs with index
+-- for i = 1, 9 do
+--   table.insert(config.keys, {
+--     key = tostring(i),
+--     mods = "LEADER",
+--     action = act.ActivateTab(i - 1)
+--   })
+-- end
+--
+-- config.key_tables = {
+--   resize_pane = {
+--     { key = "h",      action = act.AdjustPaneSize { "Left", 1 } },
+--     { key = "j",      action = act.AdjustPaneSize { "Down", 1 } },
+--     { key = "k",      action = act.AdjustPaneSize { "Up", 1 } },
+--     { key = "l",      action = act.AdjustPaneSize { "Right", 1 } },
+--     { key = "Escape", action = "PopKeyTable" },
+--     { key = "Enter",  action = "PopKeyTable" },
+--   },
+--   move_tab = {
+--     { key = "h",      action = act.MoveTabRelative(-1) },
+--     { key = "j",      action = act.MoveTabRelative(-1) },
+--     { key = "k",      action = act.MoveTabRelative(1) },
+--     { key = "l",      action = act.MoveTabRelative(1) },
+--     { key = "Escape", action = "PopKeyTable" },
+--     { key = "Enter",  action = "PopKeyTable" },
+--   }
+-- }
+--
 -- Tab bar
 -- I don't like the look of "fancy" tab bar
 config.use_fancy_tab_bar = false
@@ -184,28 +181,6 @@ wezterm.on("update-status", function (window, _)
     { Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
     { Text = " | " },
   }))
-
-  -- Current working directory
-  -- local basename = function(s)
-  --   -- Nothing a little regex can't fix
-  --   return string.gsub(s, "(.*[/\\])(.*)", "%2")
-  -- end
-  -- CWD and CMD could be nil (e.g. viewing log using Ctrl-Alt-l). Not a big deal, but check in case
-  -- local cwd = pane:get_current_working_dir()
-  -- cwd = cwd and basename(cwd) or ""
-
-  -- Current command
-  -- local cmd = pane:get_foreground_process_name()
-  -- cmd = cmd and basename(cmd) or ""
-
-  -- Time
-  -- local time = wezterm.strftime("%H:%M")
-
-  --Battery
-  --   local bat = ''
-  -- for _, b in ipairs(wezterm.battery_info()) do
-  --   bat = '🔋' .. string.format('%.0f%%', b.state_of_charge * 100)
-  -- end
 end)
 
 -- Neovim Zen mode
