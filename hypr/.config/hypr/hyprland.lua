e3ade4afaa5f801722120afb6c45a9f7757adb07
Diego Navarro <code@diegognt.dev>
2026-09-04 15:15:52 -0500

    chore: update hypr config to lua

---
+++ hypr/.config/hypr/hyprland.lua
@@ -0,0 +1,11 @@
+-- Hyprland Lua configuration
+-- Refer to the wiki for more information: https://wiki.hypr.land/Configuring/Start/
+
+require("monitors")
+require("programs")
+require("environment")
+require("look_and_feel")
+require("misc")
+require("input")
+require("keybindings")
+require("rules")
